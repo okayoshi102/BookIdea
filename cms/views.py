02ebe95053435e28dbd01be5a0855747d7ce5cb9
okayoshi102 <yoshi102193@gmail.com>
2020-09-30 20:16:53 +0900

    感想機能の追加

--- cms/views.py
+++ cms/views.py
@@ -1,8 +1,9 @@
+from django.views.generic.list import ListView
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
-from cms.models import Book
-from cms.forms import BookForm
+from cms.models import Book, Impression
+from cms.forms import BookForm, ImpressionForm
 
 
 # Create your views here.
@@ -43,5 +44,70 @@ def book_del(request,book_id):
 	book = get_object_or_404(Book, pk=book_id)
 	book.delete()
 	return redirect("cms:book_list")
+
+
+class ImpressionList(ListView):
+	"""感想の一覧"""
+	context_object_name='impressions'
+	template_name='cms/impression_list.html'
+	paginate_by=2 # 1ページに最大2件ずつでページングする
+
+	def get (self, request, *args, **kwargs):
+		book = get_object_or_404(Book, pk=kwargs['book_id']) #親の書籍を読む
+		impressions = book.impressions.all().order_by('id') #親の書籍の，子供の感想を読む
+		self.object_list=impressions
+
+		
+		context = self.get_context_data(object_list=self.object_list, book=book)
+
+		return self.render_to_response(context)
+
+
+
+def impression_edit(request, book_id, impression_id=None):
+	"""感想の編集"""
+	book = get_object_or_404(Book, pk=book_id) # 親の書籍を読む
+
+	if impression_id: #impression_idが指定されている(修正時)
+		impression = get_object_or_404(Impression, pk=impression)
+	else:  #impression_idが指定されていない(追加時)
+		impression=Impression()
+
+
+	if request.method=="POST":
+		form = ImpressionForm(request.POST, instance=impression) # POSTされたrequestデータからフォームを作成
+		if form.is_valid():
+			impression = form.save(commit=False)
+			impression.book=book
+			impression.save()
+			return redirect("cms:impression_list", book_id=book_id)
+
+	else:  #GETの時
+		form = ImpressionForm(instance=impression) # impression インスタンスからフォームを作成
+
+
+	return render(request, "cms/impression_edit.html", dict(form=form, book_id=book_id, impression_id=impression_id))
+
+
+def impression_del(request, book_id, impression_id):
+	"""感想の削除"""
+	impression = get_object_or_404(Impression, pk=impression_id)
+	impression.delete()
+	return redirect('cms:impression_list', book_id=book_id)
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
